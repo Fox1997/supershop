1a09812e8ce29c7ac6aab1929378a396e5c68355
fox1997 <1214241313@qq.com>
2021-03-09 16:39:17 +0800

    文件名规范和编程规范

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports ={
+    configureWebpack:{
+        resolve:{
+            // 配置文件夹别名
+            alias:{
+                // '@':'src'
+                'assets':'@/assets',
+                'network':'@/network',
+                'components':'@/components',
+                'common':'@common',
+                'views':'@views'
+
+            }
+        }
+    }
+}

89f707c154054982bb9cfaba272bf352f06b454d
fox1997 <1214241313@qq.com>
2021-03-10 20:34:21 +0800

    add GoodsList

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,17 @@
 <template>
-  <div id="home">
+  <div id="home" class="wrapper">
   <nav-bar class="home-nav">
     <div slot="center">购物街</div>
   </nav-bar>
   <home-swiper :banner="banner"></home-swiper>
   <recommend-view :recommend="recommend"></recommend-view>
   <feature-view/>
-  <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>  
+  <tab-control class="tab-control" :titles="['流行','新款','精选']"
+  @tabClick="tabClick"></tab-control>  
+  <goods-list :goods="goods['pop'].list"/>
+
   </div>
+ 
 </template>
 
 <script>
@@ -17,10 +21,10 @@ import FeatureView from './childComps/FeatureView'
 
 import NavBar from 'components/common/navbar/NavBar'
 import TabControl from 'components/content/tabControl/TabControl'
-
-// 没有default导出只能用{}
-import {getHomeMultidata} from "network/home"
-
+import GoodsList from 'components/content/goods/GoodsList'
+// import GoodsListItem from 'components/content/goods/GoodListItem'
+// 没有default导出只能用{},network
+import { getHomeMultidata, getHomeGoods} from "network/home"
 
 export default {
     name:"Home",
@@ -30,20 +34,44 @@ export default {
       // SwiperItem,
       RecommendView,
       FeatureView,
-      TabControl
+      TabControl,
+      GoodsList
     },
     data(){
       return{
        banner:[],
-       recommend:[]
+       recommend:[],
+       goods:{
+         'pop':{page:0,list:[]},
+         'news':{page:0,list:[]},
+         'sell':{page:0,list:[]}
+       }
       }
     },
     created(){
-      getHomeMultidata().then(res =>{
+      //请求首页显示数据
+      this.getHomeMultidata()
+      // 请求首页商品数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('news')
+      this.getHomeGoods('sell')
+    },
+    methods:{
+      getHomeMultidata(){
+        getHomeMultidata().then(res =>{
         // console.log(res);
         this.banner = res.data.banner.list;
         this.recommend= res.data.recommend.list;
       })
+      },
+      getHomeGoods(type){
+      const page = this.goods[type].page +1
+      getHomeGoods(type,page).then(res =>{
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      })
+      }   
+
     }
 }
 </script>
@@ -51,6 +79,8 @@ export default {
 <style scoped>
   #home {
     padding-top: 44px;
+       /* height: 100vh;
+    position: relative; */
   }
   .home-nav {
     background-color: var(--color-tint);
